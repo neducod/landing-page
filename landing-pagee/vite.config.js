788f2ef298ac850e793da12c86db009f99b53c70
neducod <nedunony16@gmail.com>
2025-10-13 20:57:09 +0100

    Vercel deployment

--- landing-pagee/vite.config.js
+++ landing-pagee/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 })
+
+/*
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // this is crucial
+})
+*/
