f2d158b1df532fb47d3e84014a61749e5394313a
neducod <nedunony16@gmail.com>
2025-10-15 02:26:57 +0100

    text update

--- landing-pagee/src/components/Navbar.jsx
+++ landing-pagee/src/components/Navbar.jsx
@@ -16,14 +16,17 @@ import logo from "../assets/logo.svg";
       <header className="navbar">
         <div className="nav-left">
           <img src={logo} alt="Logo" className="nav-logo" />
-          <span className="brand-name">
+          {/* <span className="brand-name">
             <strong>SEGUN</strong> <br /> ARIBIDO
+          </span> */}
+          <span className="brand-name">
+            SEGUN <br /> ARIBIDO
           </span>
         </div>
   
         <nav className="nav-right">
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
+            <li><a href="#home" className="active">Home</a></li>
             <li><a href="#work">Work</a></li>
             <li><a href="#about">About me</a></li>
           </ul>
