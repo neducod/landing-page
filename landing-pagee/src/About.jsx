91de6c28a0de4c2425bd2b3f7e6d767979754e0e
neducod <nedunony16@gmail.com>
2025-10-16 10:04:40 +0100

    testing deployment

--- landing-pagee/src/About.jsx
+++ landing-pagee/src/About.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./styles/about.css";
 
 function About() {
   return (
     <>
       <Navbar />
-      <section style={{ padding: "4rem 1rem", textAlign: "center" }}>
-        <h1>About Me</h1>
-        <p style={{ maxWidth: "600px", margin: "1rem auto", color: "#aaa" }}>
-          Hey there 👋 I'm Josephine, a frontend developer passionate about
-          crafting user-focused experiences that feel effortless and intuitive.
-          This is the About page — fully routed without reloading!
-        </p>
+      <section>
+
+
+
+
+
+
       </section>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
