864f7ead7650aa8570797b4a91948b774f06c89f
neducod <nedunony16@gmail.com>
2025-10-17 06:01:00 +0100

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,10 @@ const Footer = () => {
       <footer>
         <div className="footer-a">
           <div className="footer-aa">
-            <h2>Let’s work together</h2>
+            <h2>Let’s <br />  work together</h2>
             <div className="footer-in-content">
-              
+              <img src="/arrow-icon.png" alt="" />
+              <a href="#">aribidoolusegun@gmail.com</a>
             </div>
           </div>
           <p>Do you have an interesting project? I’ll love to hear from you. Reach out and let’s create something amazing together.</p>
